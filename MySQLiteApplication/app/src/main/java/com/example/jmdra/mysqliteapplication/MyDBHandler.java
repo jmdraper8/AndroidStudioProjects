0e64d5ff023362d5c8a1737f57cb6d1ab42bae92
John Draper <jmdraper8@gmail.com>
2016-02-21 15:41:00 +1100

    Second Checkin
    My Second Checkin

---
+++ MySQLiteApplication/app/src/main/java/com/example/jmdra/mysqliteapplication/MyDBHandler.java
@@ -0,0 +1,18 @@
+package com.example.jmdra.mysqliteapplication;
+
+/**
+ * Created by jmdra on 2/21/2016.
+ */
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+
+
+public class MyDBHandler {
+
+    
+
+}
---
+++ MySQLiteApplication/app/src/main/java/com/example/jmdra/mysqliteapplication/Products.java
@@ -0,0 +1,34 @@
+package com.example.jmdra.mysqliteapplication;
+
+/**
+ * Created by jmdra on 2/21/2016.
+ */
+public class Products {
+
+    private int _id;
+    private String _productName;
+
+    public Products() {
+
+    }
+
+    public Products(String productName) {
+        this._productName = productName;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public void set_productName(String _productName) {
+        this._productName = _productName;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String get_productName() {
+        return _productName;
+    }
+}
