c0442bbd897c6680ce769fc8e145f75065c719fb
John Draper <jmdraper8@gmail.com>
2016-02-21 16:58:58 +1100

    Final Checkin
    Final Checkin for today, DB application not functioning

--- MySQLiteApplication/app/src/main/java/com/example/jmdra/mysqliteapplication/MainActivity.java
+++ MySQLiteApplication/app/src/main/java/com/example/jmdra/mysqliteapplication/MainActivity.java
@@ -2,38 +2,54 @@ package com.example.jmdra.mysqliteapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText johnsInput;
+    TextView johnsText;
+    MyDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        johnsInput = (EditText) findViewById(R.id.johnsInput);
+        johnsText = (TextView) findViewById(R.id.johnsText);
+
+        dbHandler = new MyDBHandler(this, null, null, 1);
+
+        printDatabase();
+
+    }
+
+    //Add a product to the database
+    public void addButtonClicked(View view) {
+
+        Products product = new Products(johnsInput.getText().toString());
+
+        dbHandler.addProduct(product);
+        printDatabase();
+    }
+
+    //delete items frm database
+    public void deleteButtonClicked(View view) {
+        String inputText = johnsInput.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+    }
+
+    public void printDatabase() {
+        String dbString = dbHandler.databaseToString();
+        johnsText.setText(dbString);
+        johnsInput.setText("");
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- MySQLiteApplication/app/src/main/java/com/example/jmdra/mysqliteapplication/MyDBHandler.java
+++ MySQLiteApplication/app/src/main/java/com/example/jmdra/mysqliteapplication/MyDBHandler.java
@@ -11,8 +11,77 @@ import android.content.Context;
 import android.content.ContentValues;
 
 
-public class MyDBHandler {
+public class MyDBHandler extends SQLiteOpenHelper {
 
-    
+    //version tracking for the database, so when you make a change update the version number
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "products.db";
+    public static final String TABLE_PRODUCTS = "Products";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_PRODUCTS + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT ," +
+                COLUMN_PRODUCTNAME + " TEXT " +
+                ");";
+
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_PRODUCTS);
+        onCreate(db);
+
+    }
+
+    //Add a new row to the database
+    public void addProduct(Products products) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, products.get_productname());
+
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_PRODUCTS, null, values);
+
+        db.close();
+    }
+
+    //Delete a product from the database
+    public void deleteProduct(String productname) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM" + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + "=\"" + productname + "\";" );
+    }
+
+    //Print out the database as a string
+    public String databaseToString() {
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE 1";
+
+        //Cursor point to a location in your results
+        Cursor c = db.rawQuery(query, null);
+
+        //Move to the first row in your results
+        c.moveToFirst();
+
+        while(!c.isAfterLast()) {
+            if(c.getString(c.getColumnIndex("productname"))!= null) {
+                dbString += c.getString(c.getColumnIndex("productname"));
+                dbString += "\n";
+            }
+        }
+        db.close();
+        return dbString;
+
+    }
 
 }
